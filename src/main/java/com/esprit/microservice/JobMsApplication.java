82815750bebed2170d74030f5906d3f6aca50548
Mahjoub <oussema.mahjoub@esprit.tn>
2020-11-20 14:30:36 +0100

    .

--- src/main/java/com/esprit/microservice/JobMsApplication.java
+++ src/main/java/com/esprit/microservice/JobMsApplication.java
@@ -3,9 +3,11 @@ package com.esprit.microservice;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JobMsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/esprit/microservice/JobRepository.java
+++ src/main/java/com/esprit/microservice/JobRepository.java
@@ -1,7 +1,12 @@
 package com.esprit.microservice;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JobRepository extends JpaRepository<Job, Integer>  {
-
+	@Query("select c from Job c where c.service like :service")
+	public Page<Job> candidatByNom(@Param("service") String service,Pageable  pageable);
 }
